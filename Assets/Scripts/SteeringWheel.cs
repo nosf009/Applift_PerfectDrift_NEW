c4d2fba0807629d5746c70ed7b88463239535311
nzeman <nikola.zeman1337@gmail.com>
2021-05-28 13:49:17 +0200

    - EOL prefab changes - HcNikola

--- Assets/Scripts/SteeringWheel.cs
+++ Assets/Scripts/SteeringWheel.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public enum SteerArea { Green, Orange, Red }
+public enum SteerArea { Green, Orange, Red, Invalid }
 
 public class SteeringWheel : MonoBehaviour
 {
@@ -35,6 +35,7 @@ public class SteeringWheel : MonoBehaviour
 
         if (Mathf.Approximately(zValue, 0f))
         {
+            currentArea = SteerArea.Invalid;
             return;
         }
 
@@ -46,17 +47,19 @@ public class SteeringWheel : MonoBehaviour
                 currentArea = SteerArea.Green;
                 //Debug.Log("<color=Green>CORRECT!</color> :: zValue:" + zValue + " is bigger than LOWER BOUNDARY " + HCFW.GameManager.Instance.MenuManager.greenLowerBoundaryZ + " and smaller than HIGHER BOUNDARY " + HCFW.GameManager.Instance.MenuManager.greenHigherBoundaryZ);
             }
-
-            if (zValue > HCFW.GameManager.Instance.MenuManager.lowerBoundaryOrangeZ && zValue < HCFW.GameManager.Instance.MenuManager.higherBoundaryOrangeZ)
+            else  if (zValue > HCFW.GameManager.Instance.MenuManager.lowerBoundaryOrangeZ && zValue < HCFW.GameManager.Instance.MenuManager.higherBoundaryOrangeZ)
             {
                 currentArea = SteerArea.Orange;
                 //Debug.Log("<color=Orange>ORANGE!</color> :: zValue:" + zValue + " is bigger than LOWER BOUNDARY " + HCFW.GameManager.Instance.MenuManager.lowerBoundaryOrangeZ + " and smaller than HIGHER BOUNDARY " + HCFW.GameManager.Instance.MenuManager.higherBoundaryOrangeZ);
             }
-
-            if (zValue > HCFW.GameManager.Instance.MenuManager.lowerBoundaryRedZ && zValue < HCFW.GameManager.Instance.MenuManager.higherBoundaryRedZ)
+            else if (zValue > HCFW.GameManager.Instance.MenuManager.lowerBoundaryRedZ && zValue < HCFW.GameManager.Instance.MenuManager.higherBoundaryRedZ)
             {
                 currentArea = SteerArea.Red;
                 //Debug.Log("<color=Red>RED!</color> :: zValue:" + zValue + " is bigger than LOWER BOUNDARY  " + HCFW.GameManager.Instance.MenuManager.lowerBoundaryRedZ + " and smaller than HIGHER BOUNDARY " + HCFW.GameManager.Instance.MenuManager.higherBoundaryRedZ);
+            }
+            else
+            {
+                currentArea = SteerArea.Invalid;
             }
         }
         else
@@ -66,17 +69,19 @@ public class SteeringWheel : MonoBehaviour
                 currentArea = SteerArea.Green;
                 //Debug.Log("<color=Green>CORRECT!</color> :: zValue:" + zValue + " is bigger than LOWER BOUNDARY " + HCFW.GameManager.Instance.MenuManager.greenLowerBoundaryZ + " and smaller than HIGHER BOUNDARY " + HCFW.GameManager.Instance.MenuManager.greenHigherBoundaryZ);
             }
-
-            if (zValue < HCFW.GameManager.Instance.MenuManager.lowerBoundaryOrangeZ && zValue > HCFW.GameManager.Instance.MenuManager.higherBoundaryOrangeZ)
+            else  if (zValue < HCFW.GameManager.Instance.MenuManager.lowerBoundaryOrangeZ && zValue > HCFW.GameManager.Instance.MenuManager.higherBoundaryOrangeZ)
             {
                 currentArea = SteerArea.Orange;
                 //Debug.Log("<color=Orange>ORANGE!</color> :: zValue:" + zValue + " is bigger than LOWER BOUNDARY " + HCFW.GameManager.Instance.MenuManager.lowerBoundaryOrangeZ + " and smaller than HIGHER BOUNDARY " + HCFW.GameManager.Instance.MenuManager.higherBoundaryOrangeZ);
             }
-
-            if (zValue < HCFW.GameManager.Instance.MenuManager.lowerBoundaryRedZ && zValue > HCFW.GameManager.Instance.MenuManager.higherBoundaryRedZ)
+            else if (zValue < HCFW.GameManager.Instance.MenuManager.lowerBoundaryRedZ && zValue > HCFW.GameManager.Instance.MenuManager.higherBoundaryRedZ)
             {
                 currentArea = SteerArea.Red;
                 //Debug.Log("<color=Red>RED!</color> :: zValue:" + zValue + " is bigger than LOWER BOUNDARY  " + HCFW.GameManager.Instance.MenuManager.lowerBoundaryRedZ + " and smaller than HIGHER BOUNDARY " + HCFW.GameManager.Instance.MenuManager.higherBoundaryRedZ);
+            }
+            else
+            {
+                currentArea = SteerArea.Invalid;
             }
         }
 
